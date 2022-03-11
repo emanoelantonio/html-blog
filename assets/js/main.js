7d13d87f766b2385d0696f2733e4b14d0b233129
emanoelantonio <dev.emanoel@protonmail.com>
2022-03-11 18:14:09 -0300

    Leituras Rápidas adicionado.

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,23 @@ themeToggleBtn.addEventListener('click', () => {
   }
 });
 // Swiper
+
+const swiper = new Swiper('.swiper',{
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 2
+    },
+    1200: {
+      slidesPerView: 3
+    },
+  }
+});
